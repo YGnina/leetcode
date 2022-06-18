428d9efc134c065b78b79fd42079c37157e86278
YGnina <93010504+YGnina@users.noreply.github.com>
2022-06-18 16:50:15 -0700

    Update Solution3.c

--- two sum/Solution3.c
+++ two sum/Solution3.c
@@ -5,15 +5,15 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
 
     *returnSize=2;
     
-    int *returna=(int*)malloc(2*(sizeof(int)));
+    int *result=(int*)malloc(2*(sizeof(int)));
     
     for(int i=0;i<numsSize;i++){
       for(int j=i+1;j<numsSize;j++){
          if(nums[i]+nums[j]==target){
-             returna[0]=i;
-             returna[1]=j;
+             result[0]=i;
+             result[1]=j;
          }
       }
     }
-    return returna;
+    return result;
 }
