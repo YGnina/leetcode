8971dd62fdc23edbedcc9db9caaebbf6d92cde1e
YGnina <93010504+YGnina@users.noreply.github.com>
2022-08-11 04:40:14 +0000

    Create solution1.py

---
+++ 144. Binary Tree Preorder Traversal/solution1.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        # Store nodes
+        ans = []
+	      # Recursive function
+        def preorder_traversal(root):
+			  # Base case
+            if root is None:
+                return
+            
+			  # Record node
+            ans.append(root.val)
+			  # Travel left then right
+            preorder_traversal(root.left)
+            preorder_traversal(root.right)
+            
+        preorder_traversal(root)
+        return ans
