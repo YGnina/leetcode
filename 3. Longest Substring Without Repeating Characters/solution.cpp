9f64bd581930463cc19488b016412f5542e1b954
YGnina <93010504+YGnina@users.noreply.github.com>
2022-12-02 22:39:01 -0800

    no idea 'Sliding Window'

--- 3. Longest Substring Without Repeating Characters/solution.cpp
+++ 3. Longest Substring Without Repeating Characters/solution.cpp
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/discuss/2133524/JavaC%2B%2B-A-reall-Detailed-Explanation
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
