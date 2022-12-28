04b8d2ec1290e67842e7230a6c8a6cfe2bbcfa05
YGnina <93010504+YGnina@users.noreply.github.com>
2022-12-27 23:14:14 -0800

    easy

--- 412. Fizz Buzz/solution.java
+++ 412. Fizz Buzz/solution.java
@@ -2,13 +2,11 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> list = new ArrayList<>(n);
         for (int i = 1; i <= n; ++i) {
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-            if (divisibleBy3 && divisibleBy5) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 list.add("FizzBuzz");
-            } else if (divisibleBy3) {
+            } else if (i % 3 == 0) {
                 list.add("Fizz");
-            } else if (divisibleBy5) {
+            } else if (i % 5 == 0) {
                 list.add("Buzz");
             } else {
                 list.add(String.valueOf(i));
