784feb453336a013608b21580267079e5a85c810
YGnina <93010504+YGnina@users.noreply.github.com>
2023-07-04 11:59:27 -0400

    Easy//Top Interview Questions

--- 387. First Unique Character in a String/solution.cpp
+++ 387. First Unique Character in a String/solution.cpp
@@ -2,11 +2,7 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         int len = s.size();
-
-        if(len == 1){
-            return -1;
-        }
-
+        
         for(int i=0; i<len;i++){
             int flag = 1;
             for(int j=0; j<len; j++){
