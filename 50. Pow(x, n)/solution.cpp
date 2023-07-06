e17ce41507580e49a9d20a5af601390531fcf1df
YGnina <93010504+YGnina@users.noreply.github.com>
2023-07-06 18:24:20 -0400

    Medium//Top Interview Questions

--- 50. Pow(x, n)/solution.cpp
+++ 50. Pow(x, n)/solution.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    double binaryExp(double x, long long n) {
-        // Base case
-        if (n == 0) {
+    double myPow(double x, int n) {
+        //base case
+        if(n==0){
             return 1;
         }
 
-        if(n == 1){
+        if(n==1){
             return x;
         }
-       
-        // Handle case where, n < 0.
-        if (n < 0) {
-            return 1.0 / binaryExp(x, -1 * n);
-        }
-       
-        // Perform Binary Exponentiation.
-        // If 'n' is odd we perform Binary Exponentiation on 'n - 1' and multiply result with 'x'.
-        if (n % 2 == 1) {
-            return x * binaryExp(x * x, (n - 1) / 2);
-        }
-        // Otherwise we calculate result by performing Binary Exponentiation on 'n'.
-        else {
-            return binaryExp(x * x, n / 2);
-        }
-    }
 
-    double myPow(double x, int n) {
-        return binaryExp(x, (long long) n);
+        // Perform Binary Exponentiation.
+        if(n>0){
+            // If 'n' is odd we perform Binary Exponentiation on 'n - 1' and multiply result with 'x'.
+            if(n%2==0){
+                return myPow(x*x, n/2);
+            }
+            // Otherwise we calculate result by performing Binary Exponentiation on 'n'.
+            else{
+                return x*myPow(x*x, n/2);
+            }
+        }else{ // negative power = inverse
+            n= abs(n);
+            if(n%2==0){
+                return 1/myPow(x*x, n/2);
+            }
+            else{
+                return 1/(x*myPow(x*x, n/2));
+            }
+        }
     }
 };
