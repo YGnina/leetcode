2ca1be5260516a519aabfd55d0ed951925fcd2d8
YGnina <93010504+YGnina@users.noreply.github.com>
2023-08-01 19:35:58 -0400

    Medium//Top Interview Questions//Brute Force

---
+++ 73. Set Matrix Zeroes/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();      //column
+        int m = matrix[0].size();   //row
+
+        vector<vector<int>> visited = matrix;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    for(int k=0; k<m; k++){
+                        visited[i][k] = 0;
+                    }
+                    for(int k=0; k<n; k++){
+                        visited[k][j] = 0;
+                    }
+                    
+                }
+            }
+        }
+        
+         for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                matrix[i][j] = visited[i][j];
+            }
+        }
+    }
+};
