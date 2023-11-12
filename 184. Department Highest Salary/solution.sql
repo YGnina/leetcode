0aac5b74e5a9d858169874b38fb91ac085a928b7
YGnina <93010504+YGnina@users.noreply.github.com>
2023-11-11 21:02:01 -0500

    sql database medium

---
+++ 184. Department Highest Salary/solution.sql
@@ -0,0 +1,5 @@
+SELECT d.name AS Department, e.name AS Employee, e.salary
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE (departmentId, salary) IN 
+  (SELECT departmentId, MAX(salary) FROM Employee GROUP BY departmentId)
