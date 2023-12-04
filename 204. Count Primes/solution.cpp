23e0b07d05862db7c48177e987c602b89567a1d3
YGnina <93010504+YGnina@users.noreply.github.com>
2023-12-03 20:44:05 -0500

    Medium//Top Interview Questions

--- 204. Count Primes/solution.cpp
+++ 204. Count Primes/solution.cpp
@@ -5,7 +5,9 @@ public:
         if(n==0 || n==1){
             return 0;
         }
-        
+
+        // Sieve of Eratosthenes  埃拉托斯特尼筛法
+        // 要得到自然数n以内的全部素数，必须把不大于√n的所有素数的倍数剔除，剩下的就是素数。
         vector<bool> prime(n+1, true);
         prime[0] = false;
         prime[1] = true;
