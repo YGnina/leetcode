1c01c2f9249b0299aa7852a589a0a94a6c145e7b
YGnina <93010504+YGnina@users.noreply.github.com>
2023-12-24 21:24:29 -0500

    Easy//arrays 101

---
+++ 1051. Height Checker/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> expected = heights;
+        sort(heights.begin(),heights.end());
+        
+        int ans = 0;
+        for(int i=0; i<heights.size();i++){
+            if(expected[i] != heights[i]){
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
