0931a55637300ae4ed89431fe9c5c55335321c66
YGnina <93010504+YGnina@users.noreply.github.com>
2024-01-27 20:03:35 -0500

    Easy//Binary Search **Review

--- 69. Sqrt(x)/solution.cpp
+++ 69. Sqrt(x)/solution.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        return sqrt(x);
+  int mySqrt(int x) {      
+    long l = 1;
+    long r = static_cast<long>(x) + 1;
+    while (l < r) {
+      long m = l + (r - l) / 2;
+      if (m > x/m) { 
+        r = m;
+      } else {
+        l = m + 1;
+      }
     }
+    // l: smallest number such that l * l > x
+    return l - 1;
+  }
 };
