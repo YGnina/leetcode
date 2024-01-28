3c56981f360ee18f9d75b4689f8b4f2a7cbfcca1
YGnina <93010504+YGnina@users.noreply.github.com>
2024-01-27 19:58:54 -0500

    Easy//Binary Search

---
+++ 278. First Bad Version/solution.cpp
@@ -0,0 +1,25 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+//1,2,3,4,5
+//g,g,g,b,b
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        
+        while(low < high){
+            int mid = low + (high-low)/2;
+            if(!isBadVersion(mid)){
+                low = mid + 1;
+            }else{
+                if(!isBadVersion(mid-1)){
+                    return mid;
+                }
+                high = mid;
+            }
+        }
+        return low;
+    }
+};
