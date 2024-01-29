36d7dc232028b45c0e791bd81020436f9f105c07
YGnina <93010504+YGnina@users.noreply.github.com>
2024-01-28 21:12:43 -0500

    Easy//Binary Search

--- 367. Valid Perfect Square/solution.cpp
+++ 367. Valid Perfect Square/solution.cpp
@@ -1,17 +1,24 @@
 class Solution {
- public:
-  bool isPerfectSquare(int num) {
-    long l = 1;
-    long r = num;
-
-    while (l < r) {
-      const long m = (l + r) / 2;
-      if (m >= num / m)
-        r = m;
-      else
-        l = m + 1;
+public:
+    bool isPerfectSquare(int num) {
+        
+        if(num == 1){
+            return true;
+        }
+        
+        int l = 0;
+        int h = num/2;
+        while(l<=h){
+            long int mid = l + (h-l)/2;
+            if((long long)mid*mid == num){
+                return true;
+            } 
+            if((long long)mid*mid > num){
+                h = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return false;
     }
-
-    return l * l == num;
-  }
 };
